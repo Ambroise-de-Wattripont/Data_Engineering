b000763d38a08f11ad762a71a9664b9bcf12b122
Ambroise-de-Wattripont <66125922+Ambroise-de-Wattripont@users.noreply.github.com>
2023-07-13 14:46:37 +0200

    Avancement front

--- src/views/Connexion_recruteur.vue
+++ src/views/Connexion_recruteur.vue
@@ -1,11 +1,26 @@
-<template>
-  <h1>
-    adresse mail
-    mot de passe
-    soucis de co
-    
-    <RouterLink to="/profil_postulant">Connexion</RouterLink>
-    <RouterLink to="/inscription">Inscription</RouterLink>
+<script lang="ts">
+  const emailR="exemple@gmail.com"
+  const passewordR=String
+</script>
 
-  </h1>
+<template>
+
+  <div id="connexion">
+      <p>Adresse mail {{ emailR }}</p>
+          <input v-model="emailR"/>
+       <p>Mot de passe {{ passwordR }}</p>
+    <input v-model="passewordR"/>
+  </div>
+
+
+<RouterLink to="/profil_recruteur">
+  <button> Connexion </button>
+</RouterLink>
+
+<p>Probleme de connexion ?</p>
+<p>Pas de compte ?</p>
+<RouterLink to="/inscription">
+  <button>Inscription</button>
+</RouterLink>
+
 </template>
